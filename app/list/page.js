864e129c9b46a313ae28137eec81c53c3cc1ac7a
SH222 <k.olsh222@gmail.com>
2023-05-16 12:14:42 +0900

    build: /list dynamic으로 변경

--- app/list/page.js
+++ app/list/page.js
@@ -2,6 +2,8 @@ import { connectDB } from "@/util/database";
 import Link from "next/link";
 import ListItem from "./listItem";
 
+export const dynamic = "force-dynamic";
+
 export default async function List() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { connectDB } from "@/util/database";
 export default async function Home() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray(); // post collection에 있는 모든 데이터를 array 형식으로 가져옴
-  // console.log(result);
 
   return <div>Hello</div>;
 }
+await fetch("/URL", { next: { revalidate: 60 } });
